675fcfa8a5756a08ba0f57e3177a1e42d54dff3b
JeffRisberg <jrisberg@aol.com>
2022-03-23 11:05:55 -0700

    set up example

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ var globSync = require('glob').sync;
 var express = require('express');
 var app = express();
 
-var mocks = globSync('./mocks/**/*.js', {cwd: __dirname}).map(require);
-
 const PATH_STYLES = path.resolve(__dirname, '../app/styles');
 const PATH_DIST = path.resolve(__dirname, '../dist');
 
@@ -13,15 +11,6 @@ app.use(express.static(PATH_DIST));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../app/index.html'));
-});
-
-var nedb = require('nedb');
-
-app.itemsDB = new nedb({filename: 'db-content/items', autoload: true});
-app.eventsDB = new nedb({filename: 'db-content/events', autoload: true});
-
-mocks.forEach(function (route) {
-    route(app);
 });
 
 var server = app.listen(process.env.PORT || 3000, () => {
