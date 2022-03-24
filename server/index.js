78124c095eefa830744857370d138fd5dda180ab
JeffRisberg <jrisberg@aol.com>
2022-03-24 14:29:02 -0700

    improved organization

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 var path = require('path');
-var globSync = require('glob').sync;
 var express = require('express');
 var app = express();
 
 const PATH_STYLES = path.resolve(__dirname, '../app/styles');
-const PATH_DIST = path.resolve(__dirname, '../dist');
+//const PATH_DIST = path.resolve(__dirname, '../dist');
 
 app.use('/styles', express.static(PATH_STYLES));
-app.use(express.static(PATH_DIST));
+//app.use(express.static(PATH_DIST));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../app/index.html'));
-});
+//app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../app/index.html'));
+//});
+app.use('/', express.static(path.join(__dirname, '..', 'build')));
 
 var server = app.listen(process.env.PORT || 3000, () => {
     var port = server.address().port;
