e650aff1beb12a07e75666d5ada132310fd15f11
JeffRisberg <j.s.risberg@gmail.com>
2022-03-25 17:36:40 -0700

    first running example

--- server/index.js
+++ server/index.js
@@ -3,14 +3,9 @@ var express = require('express');
 var app = express();
 
 const PATH_STYLES = path.resolve(__dirname, '../app/styles');
-//const PATH_DIST = path.resolve(__dirname, '../dist');
 
 app.use('/styles', express.static(PATH_STYLES));
-//app.use(express.static(PATH_DIST));
 
-//app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../app/index.html'));
-//});
 app.use('/', express.static(path.join(__dirname, '..', 'build')));
 
 var server = app.listen(process.env.PORT || 3000, () => {
